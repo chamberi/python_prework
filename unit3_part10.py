fe5386e270a352a9b057b8b8085c249e738b5bde
Colin Lamont <colinlamont@gmail.com>
2016-11-22 15:07:35 -0800

    unit 3, part 10 done

--- unit3_part10.py
+++ unit3_part10.py
@@ -6,7 +6,7 @@ if len(original) > 0 and original.isalpha():
     print original
     word = original.lower()
     first = word[0]
-    new_word = original[1:len(new_word)-1] + first + pyg
+    new_word = word[1:len(word)-1] + first + pyg
     print new_word
 else:
     print 'empty'
